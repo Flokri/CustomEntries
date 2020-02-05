0011ff6156961fc8c2059acf16da61395c56f33a
Florian Kriegl <florian.kriegl@ebcont.com>
2020-02-05 11:16:16 +0100

    Added the logic for the floating label entry

--- src/CustomEntries/shared/extensions/ViewExtensions.cs
+++ src/CustomEntries/shared/extensions/ViewExtensions.cs
@@ -8,9 +8,7 @@ namespace CustomEntries
 {
     public static class ViewExtensions
     {
-        private const string AnimationName = "ColorTo";
-
-        public static Task<bool> ColorTo (this VisualElement self, Color startColor, Color endColor, Action<Color> callback, uint length = 200, Easing easing = null)
+        public static Task<bool> ColorTo(this VisualElement self, Color startColor, Color endColor, Action<Color> callback, uint length = 200, Easing easing = null)
         {
             Func<double, Color> transform = (t) =>
             Color.FromRgba(startColor.R + t * (endColor.R - startColor.R),
@@ -18,17 +16,26 @@ namespace CustomEntries
             startColor.B + t * (endColor.B - startColor.B),
             startColor.A + t * (endColor.A - startColor.A));
 
-            return ColorAnimation(self, transform, callback, length, easing);
+            return ColorAnimation(self, "ColorTo", transform, callback, length, easing);
         }
 
-        public static void CancelAnimation(this VisualElement self) => self.AbortAnimation(AnimationName);
+        public static void CancelAnimation(this VisualElement self, string animationName) => self.AbortAnimation(animationName);
 
-        static Task<bool> ColorAnimation(VisualElement element, Func<double, Color> transform, Action<Color> callback, uint length, Easing easing)
+        static Task<bool> ColorAnimation(VisualElement element, string animationName, Func<double, Color> transform, Action<Color> callback, uint length, Easing easing)
         {
             easing = easing ?? Easing.Linear;
             var taskCompletionSource = new TaskCompletionSource<bool>();
 
-            element.Animate<Color>(AnimationName, transform, callback, 16, length, easing, (v, c) => taskCompletionSource.SetResult(c));
+            element.Animate<Color>(animationName, transform, callback, 16, length, easing, (v, c) => taskCompletionSource.SetResult(c));
+            return taskCompletionSource.Task;
+        }
+
+        public static Task<bool> SizeTo(this VisualElement self, double startSize, double endSize, Action<double> callback, uint length, Easing easing = null)
+        {
+            easing = easing ?? Easing.Linear;
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+            self.Animate("SizeTo", callback, startSize, endSize, 16, length, easing, (v, c) => taskCompletionSource.SetResult(c));
+
             return taskCompletionSource.Task;
         }
     }
--- src/CustomEntries/shared/views/FloatingLabelEntry.xaml.cs
+++ src/CustomEntries/shared/views/FloatingLabelEntry.xaml.cs
@@ -12,9 +12,131 @@ namespace CustomEntries
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FloatingLabelEntry : ContentView
     {
+        #region instances
+        const int TITLE_FONT_SIZE = 14;
+        const int PLACEHOLDER_FONT_SIZE = 18;
+
+        public event EventHandler Completed;
+        #endregion
+
+        #region BindablePropeties
+        public readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(FloatingLabelEntry), string.Empty);
+        public readonly BindableProperty PlaceholderProperty = BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(FloatingLabelEntry), string.Empty);
+        public readonly BindableProperty DefaultTextColorProperty = BindableProperty.Create(nameof(DefaultTextColor), typeof(Color), typeof(FloatingLabelEntry), Color.Gray);
+        public readonly BindableProperty ActiveTextColorProperty = BindableProperty.Create(nameof(ActiveTextColor), typeof(string), typeof(FloatingLabelEntry), Color.Gray);
+        public readonly BindableProperty DefaultBorderColorProperty = BindableProperty.Create(nameof(DefaultBorderColor), typeof(string), typeof(FloatingLabelEntry), Color.Gray);
+        public readonly BindableProperty ActiveBorderColorProperty = BindableProperty.Create(nameof(ActiveBorderColor), typeof(string), typeof(FloatingLabelEntry), Color.Gray);
+        public readonly BindableProperty AnimatedProperty = BindableProperty.Create(nameof(Animated), typeof(bool), typeof(FloatingLabelEntry), true);
+
+        #endregion
+
+        #region constructor
         public FloatingLabelEntry()
         {
             InitializeComponent();
+
+            PlaceholderLabel.TranslationX = 10;
+            PlaceholderLabel.FontSize = PLACEHOLDER_FONT_SIZE;
         }
+        #endregion
+
+        #region event handler
+        async void HandleFocused(object sender, FocusEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                await PlaceholderToTitle();
+            }
+        }
+
+        async void HandleUnfocused(object sender, FocusEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                await TitleToPlaceholder();
+            }
+        }
+
+        async void HandleTapped(object sender, EventArgs e)
+        {
+            if (IsEnabled)
+                BorderlessEntry.Focus();
+        }
+
+        void HandleCompleted(object sender, EventArgs e)
+        {
+            Completed?.Invoke(this, e);
+        }
+        #endregion
+
+        #region privates
+        async Task PlaceholderToTitle()
+        {
+            if (Animated)
+            {
+                await Task.WhenAll(
+                    PlaceholderLabel.TranslateTo(0, BorderlessEntry.Y - PlaceholderLabel.Height, 100),
+                    PlaceholderLabel.SizeTo(PlaceholderLabel.FontSize, TITLE_FONT_SIZE, (t) => PlaceholderLabel.FontSize = t, 100, Easing.BounceIn),
+                    PlaceholderLabel.ColorTo(DefaultTextColor, ActiveTextColor, (c)=>PlaceholderLabel.TextColor = c,100),
+                    HiddenBottomBorder.LayoutTo(new Rectangle(BottomBorder.X, BottomBorder.Y, BottomBorder.Width, BottomBorder.Height), 100));
+            }
+        }
+
+        async Task TitleToPlaceholder()
+        {
+            if (Animated)
+            {
+                await Task.WhenAll(
+                    PlaceholderLabel.TranslateTo(10, 0, 100),
+                    PlaceholderLabel.SizeTo(PlaceholderLabel.FontSize, PLACEHOLDER_FONT_SIZE, (t) => PlaceholderLabel.FontSize = t, 100, Easing.BounceIn),
+                    PlaceholderLabel.ColorTo(ActiveTextColor, DefaultTextColor, (c) => PlaceholderLabel.TextColor = c, 100),
+                    HiddenBottomBorder.LayoutTo(new Rectangle(BottomBorder.X, BottomBorder.Y, 0, BottomBorder.Height), 100));
+            }
+        }
+        #endregion
+
+        #region properties
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public Color DefaultTextColor
+        {
+            get => (Color)GetValue(DefaultTextColorProperty);
+            set => SetValue(DefaultTextColorProperty, value);
+        }
+
+        public Color ActiveTextColor
+        {
+            get => (Color)GetValue(ActiveTextColorProperty);
+            set => SetValue(ActiveTextColorProperty, value);
+        }
+
+        public Color DefaultBorderColor
+        {
+            get => (Color)GetValue(DefaultBorderColorProperty);
+            set => SetValue(DefaultBorderColorProperty, value);
+        }
+
+        public Color ActiveBorderColor
+        {
+            get => (Color)GetValue(ActiveBorderColorProperty);
+            set => SetValue(ActiveBorderColorProperty, value);
+        }
+
+        public bool Animated
+        {
+            get => (bool)GetValue(AnimatedProperty);
+            set => SetValue(AnimatedProperty, value);
+        }
+        #endregion
     }
 }
